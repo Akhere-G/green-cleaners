293f04142f357dde23a1672838629a30cf531857
Akhere Ihoeghinlan <101akhere5@gmail.com>
2024-05-19 18:08:14 +0100

    Basic layout

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,68 @@
+import Link from "next/link";
+import React from "react";
+
+type SectionLink = {
+  title: string;
+  link: string | null;
+};
+type FooterSectionProps = {
+  title: string;
+  links: SectionLink[];
+};
+const FooterSection = ({ title, links }: FooterSectionProps) => {
+  return (
+    <section className="mb-4 pr-2">
+      <h2 className="text-xl border-b-2 border-green-800 mb-2">{title}</h2>
+      <nav>
+        <ul>
+          {links.map(({ title, link }) => {
+            return link ? <Link href={link}>{title}</Link> : <p>{title}</p>;
+          })}
+        </ul>
+      </nav>
+    </section>
+  );
+};
+export default function Footer() {
+  const areasLinks: FooterSectionProps = {
+    title: "Areas we cover",
+    links: [
+      { title: "North London", link: null },
+      { title: "South London", link: null },
+      { title: "West London", link: null },
+      { title: "East London", link: null },
+    ],
+  };
+
+  const servicesLinks: FooterSectionProps = {
+    title: "Our Services",
+    links: [
+      { title: "Daily Office Cleaning", link: null },
+      { title: "Window Cleaning", link: null },
+      { title: "Washroom Cleaning", link: null },
+      { title: "Apartment Cleaning", link: null },
+    ],
+  };
+
+  const contactUsLinks: FooterSectionProps = {
+    title: "Contact Us",
+    links: [
+      { title: "Twitter", link: null },
+      { title: "Instagram", link: null },
+      { title: "Facebook", link: null },
+    ],
+  };
+
+  return (
+    <header className=" bg-green-900 text-white py-4 px-2">
+      <div className="max-w-6xl mx-auto">
+        <h1 className="text-2xl font-bold bold mb-4">Green Cleaners</h1>
+        <nav className="grid grid-cols-2 sm:grid-cols-3 grid-">
+          <FooterSection {...areasLinks} />
+          <FooterSection {...servicesLinks} />
+          <FooterSection {...contactUsLinks} />
+        </nav>
+      </div>
+    </header>
+  );
+}
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  const links = [
+    { title: "Home", link: "/" },
+    { title: "About us", link: "/about" },
+    { title: "Contact", link: "/contact" },
+    { title: "Services", link: "/services" },
+  ];
+
+  return (
+    <header className="bg-white py-4 px-2">
+      <div className="max-w-6xl mx-auto flex flex-col items-center sm:flex-row sm:items-baseline sm:justify-between">
+        <h1 className="text-2xl font-bold text-green-900 bold">
+          Green Cleaners
+        </h1>
+        <nav>
+          <ul className="gap-2 flex">
+            {links.map(({ title, link }) => (
+              <Link href={link}>{title}</Link>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}

bf01481016d6f0622d3e4fc717220bc1ba926225
Akhere Ihoeghinlan <101akhere5@gmail.com>
2024-05-19 18:39:17 +0100

    Added hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="max-w-6xl mx-auto px-1 py-4">We are green cleaners!</main>
+    <section className="max-w-6xl mx-auto px-1 pb-4">
+      <div className="absolute w-screen left-0 h-52 object-left-top filter">
+        <Image
+          className="object-cover brightness-50"
+          src="/cleaners.jpg"
+          alt="cleaners"
+          fill
+        />
+        <div className="absolute bottom-1/2 right-1/2 translate-x-1/2 text-gray-50">
+          <h2 className="text-xl font-bold">We are Green Cleaners</h2>
+          <small>Here to provide eco-friendly cleaning services</small>
+        </div>
+      </div>
+    </section>
   );
 }

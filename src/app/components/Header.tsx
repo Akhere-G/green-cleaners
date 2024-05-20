0d07e208c0923ba353737f44367579db2498a7be
Akhere Ihoeghinlan <101akhere5@gmail.com>
2024-05-20 01:13:57 +0100

    Added icon

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import React from "react";
+import icon from "/public/icon.png";
+import Image from "next/image";
 
 export default function Header() {
   const links = [
@@ -12,9 +14,18 @@ export default function Header() {
   return (
     <header className="bg-white py-4 px-2 ">
       <div className="max-w-6xl mx-auto flex flex-col items-center sm:flex-row sm:items-baseline sm:justify-between">
-        <h1 className="text-2xl font-bold text-green-900 bold">
-          Green Cleaners
-        </h1>
+        <div className="flex items-baseline gap-2">
+          <Image
+            className="translate-y-[2px]"
+            src={icon}
+            alt="icon"
+            width={24}
+            height={24}
+          />
+          <h1 className="text-2xl font-bold text-green-900 bold">
+            Green Cleaners
+          </h1>
+        </div>
         <nav>
           <ul className="gap-2 flex">
             {links.map(({ title, link }) => (
